cd637f32e5634b06f06673639eb5c87843b56daf
DatLe3009 <105985045+DatLe3009@users.noreply.github.com>
2024-03-06 11:13:27 +0700

    add views for root api

--- src/index.js
+++ src/index.js
@@ -20,9 +20,12 @@ app.use('/', express.static(path.join(__dirname, '../public')));
 // Routes
 app.use('/', require('./routes/root'));
 app.all('*', (req, res) => {
-    res.status(404);
-    if (req.accepts('json')) {
-        res.json({ error: '404 Not Found' });
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ error: "404 Not Found" });
+    } else {
+        res.type('txt').send("404 Not Found");
     }
 })
 
--- src/routes/root.js
+++ src/routes/root.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const route = express.Router();
+const path = require('path');
+
+route.get('^/$|index(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
+});
 
 route.use('/', require('./userRoute'));
 route.use('/', require('./profileRoute'));
